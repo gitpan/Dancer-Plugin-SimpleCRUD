fb62d5dd5754e6adf6c8d6d9261ee502b34e9ccf
David Precious <davidp@preshweb.co.uk>
2012-12-12 10:31:25 +0000

    Import of BIGPRESH/Dancer-Plugin-SimpleCRUD-0.80 from CPAN.
    gitpan-cpan-distribution: Dancer-Plugin-SimpleCRUD
    gitpan-cpan-version:      0.80
    gitpan-cpan-path:         BIGPRESH/Dancer-Plugin-SimpleCRUD-0.80.tar.gz
    gitpan-cpan-author:       BIGPRESH
    gitpan-cpan-maturity:     released

--- example/simplecrud-example.pl
+++ example/simplecrud-example.pl
@@ -7,6 +7,7 @@ use Dancer::Plugin::SimpleCRUD;
 simple_crud(
     record_title => 'Person',
     db_table => 'people',
+    db_connection_name => 'foo',
     prefix => '/people',
     acceptable_values => {
         gender => [ qw( Male Female ) ],
@@ -20,6 +21,16 @@ simple_crud(
             table        => 'employer',
             key_column   => 'id',
             label_column => 'name',
+        },
+    },
+    custom_columns => {
+        mailto_link => {
+            raw_column => 'email',
+            transform  => sub { my $email = shift; return "<a href='mailto:$email'>mail</a>"; },
+        },
+        full_name => {
+            raw_column => "(first_name || ' ' || last_name)",
+            transform => sub { return shift }, # (unnecessary, btw, as this is the default)
         },
     },
 );
